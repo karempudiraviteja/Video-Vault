ddec2f87f987c922e5a5492cc9f72340f304090f
karempudiraviteja <2200030562@kluniversity.in>
2026-01-29 21:21:10 +0530

    updated client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // API client instance
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://video-vault-72q0.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
